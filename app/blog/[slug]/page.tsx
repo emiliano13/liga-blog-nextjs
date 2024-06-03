c900557a46db8549c3c3f9bba72239c65ad221dd
emiliano vera <emilianovera19@MBP-emiliano-19.fibertel.com.ar>
2024-06-03 12:07:23 -0300

    noticias relacionadas

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,14 @@
 import CommentForm from '@/components/CommentForm'
 import getAllPosts from '@/lib/queries/getAllPosts'
 import getPostBySlug from '@/lib/queries/getPostBySlug'
+import {gql} from '@apollo/client'
 import {Metadata} from 'next'
+import Image from 'next/image'
 import Link from 'next/link'
+
 import {notFound} from 'next/navigation'
+//import {useSuspenseQuery} from '@apollo/experimental-nextjs-app-support/ssr'
+import {getClient} from '@/lib/client'
 
 import {Roboto} from 'next/font/google'
 
@@ -13,6 +18,48 @@ const roboto = Roboto({
   subsets: ['latin'],
   display: 'swap'
 })
+const GET_QUERY = gql`
+  query Query($currentPostId: [ID]) {
+    posts(
+      first: 3
+      where: {orderby: {field: DATE, order: DESC}, notIn: $currentPostId}
+    ) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        commentCount
+        databaseId
+        date
+        modified
+        title
+        slug
+        excerpt(format: RENDERED)
+        featuredImage {
+          node {
+            altText
+            sourceUrl
+            mediaDetails {
+              height
+              width
+              sizes {
+                height
+                name
+                width
+                sourceUrl
+              }
+            }
+          }
+        }
+        seo {
+          metaDesc
+          title
+        }
+      }
+    }
+  }
+`
 /**
  * Generate the static routes at build time.
  *
@@ -65,73 +112,119 @@ export async function generateMetadata({
 export default async function Post({params}: {params: {slug: string}}) {
   // Fetch a single post from WordPress.
   const post = await getPostBySlug(params.slug)
-
   // No post? Bail...
   if (!post) {
     notFound()
   }
+  //Relacionadas
+  const {data} = await getClient().query({
+    query: GET_QUERY,
+    variables: {currentPostId: post.databaseId}
+  })
+  //console.log(post.databaseId)
 
   return (
-    <article className={roboto.className}>
-      <header>
-        <h2 dangerouslySetInnerHTML={{__html: post.title}} />
-        <p className="italic">
-          By {post.author.node.name} on <time>{post.date}</time>
-        </p>
-      </header>
-      <div dangerouslySetInnerHTML={{__html: post.content}} />
-      <footer className="flex items-center justify-between gap-4 pb-4">
-        <div>
-          <h3>Categories</h3>
-          <ul className="m-0 flex list-none gap-2 p-0">
-            {post.categories.nodes.map((category) => (
-              <li className="m-0 p-0" key={category.databaseId}>
-                <Link href={`/blog/category/${category.name}`}>
-                  {category.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className="grid grid-cols-4 p-2 gap-2">
+      <div className="col-1 col-span-3  p-2">
+        <article className={roboto.className}>
+          <header>
+            <h2 dangerouslySetInnerHTML={{__html: post.title}} />
+            <p className="italic">
+              Por {post.author.node.name} el <time>{post.date}</time>
+            </p>
+          </header>
+          <div dangerouslySetInnerHTML={{__html: post.content}} />
+          <footer className="flex items-center justify-between gap-4 pb-4">
+            <div>
+              <h3>Categorias</h3>
+              <ul className="m-0 flex list-none gap-2 p-0">
+                {post.categories.nodes.map((category) => (
+                  <li className="m-0 p-0" key={category.databaseId}>
+                    <Link href={`/blog/category/${category.name}`}>
+                      {category.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
 
-        <div>
-          <h3>Tags</h3>
-          <ul className="m-0 flex list-none gap-2 p-0">
-            {post.tags.nodes.map((tag) => (
-              <li className="m-0 p-0" key={tag.databaseId}>
-                <Link href={`/blog/tag/${tag.name}`}>{tag.name}</Link>
-              </li>
+            <div>
+              <h3>Tags</h3>
+              <ul className="m-0 flex list-none gap-2 p-0">
+                {post.tags.nodes.map((tag) => (
+                  <li className="m-0 p-0" key={tag.databaseId}>
+                    <Link href={`/blog/tag/${tag.name}`}>{tag.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </footer>
+          <section className="border-t-2">
+            <h3>Comments</h3>
+            {post.comments.nodes.map((comment) => (
+              <article key={comment.databaseId}>
+                <header className="flex items-center gap-2">
+                  <img
+                    alt={comment.author.node.name}
+                    className="m-0 rounded-full"
+                    height={64}
+                    loading="lazy"
+                    src={comment.author.node.avatar.url}
+                    width={64}
+                  />
+                  <div className="flex flex-col gap-2">
+                    <h4
+                      className="m-0 p-0 leading-none"
+                      dangerouslySetInnerHTML={{
+                        __html: comment.author.node.name
+                      }}
+                    />
+                    <time className="italic">{comment.date}</time>
+                  </div>
+                </header>
+
+                <div dangerouslySetInnerHTML={{__html: comment.content}} />
+              </article>
             ))}
-          </ul>
-        </div>
-      </footer>
-      <section className="border-t-2">
-        <h3>Comments</h3>
-        {post.comments.nodes.map((comment) => (
-          <article key={comment.databaseId}>
-            <header className="flex items-center gap-2">
-              <img
-                alt={comment.author.node.name}
-                className="m-0 rounded-full"
-                height={64}
-                loading="lazy"
-                src={comment.author.node.avatar.url}
-                width={64}
-              />
-              <div className="flex flex-col gap-2">
-                <h4
-                  className="m-0 p-0 leading-none"
-                  dangerouslySetInnerHTML={{__html: comment.author.node.name}}
+          </section>
+          <CommentForm postID={post.databaseId} />
+        </article>
+      </div>
+      <div className="col-2 p-2">
+        <div className="grid grid-cols-1 gap-8">
+          {data &&
+            data.posts.nodes.map((latestPost) => (
+              <article
+                className="w-1/4_ rounded overflow-hidden shadow-lg hover:shadow-2xl"
+                key={latestPost.databaseId}
+              >
+                <Image
+                  className="w-full my-0"
+                  alt={latestPost.featuredImage?.node.altText}
+                  height={
+                    latestPost.featuredImage?.node.mediaDetails.sizes[0].height
+                  }
+                  src={
+                    latestPost.featuredImage?.node.mediaDetails.sizes[0]
+                      .sourceUrl
+                  }
+                  width={
+                    latestPost.featuredImage?.node.mediaDetails.sizes[0].width
+                  }
+                  priority={true}
                 />
-                <time className="italic">{comment.date}</time>
-              </div>
-            </header>
+                <div className="px-6 py-2">
+                  <Link href={`/blog/${latestPost.slug}`}>
+                    <h2 dangerouslySetInnerHTML={{__html: latestPost.title}} />
+                  </Link>
+                  <div dangerouslySetInnerHTML={{__html: latestPost.excerpt}} />
+                </div>
 
-            <div dangerouslySetInnerHTML={{__html: comment.content}} />
-          </article>
-        ))}
-      </section>
-      <CommentForm postID={post.databaseId} />
-    </article>
+                {/* <ShareButtons post={post} /> */}
+              </article>
+            ))}
+        </div>
+      </div>
+    </div>
   )
 }
